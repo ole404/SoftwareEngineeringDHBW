6419c514304373bba3a81873c7948af8bb348bfc
ShirkahnW <64583398+ShirkahnW@users.noreply.github.com>
2022-01-09 13:48:43 +0100

    Initialise Database

---
+++ server/Schemas/treeSchema.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+interface GeoInfo {
+  address: string;
+  lat: number;
+  lon: number;
+}
+
+export interface Tree {
+  treeId: string;
+  userName: string;
+  treeName: string;
+  eloRating: number;
+  geoInfo: GeoInfo;
+  image: string;
+}
+
+export const treeSchema = new Schema<Tree>({
+  treeId: String,
+  userName: String,
+  treeName: String,
+  eloRating: Number,
+  geoInfo: { address: String, lat: Number, lon: Number },
+  image: String,
+});
