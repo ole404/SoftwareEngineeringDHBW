f1b94b1b7b57c13529f132cffa2811dc49a03c02
amon-schneider <wi19102@lehre.dhbw-stuttgart.de>
2022-01-09 16:16:12 +0100

    add back button to map-page

--- client/src/app/map/map.page.ts
+++ client/src/app/map/map.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { Map, tileLayer, marker, icon } from 'leaflet';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-map',
@@ -9,11 +8,8 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./map.page.scss'],
 })
 export class MapPage implements OnInit {
-  constructor(
-    public plt: Platform
-    ) {}
-  ngOnInit() {
-  }
+  constructor(public plt: Platform) {}
+  ngOnInit() {}
   //ngAfterViewInit(): void{}
   //ionViewDidLoad(): void{}
   ionViewDidEnter() {
@@ -23,15 +19,16 @@ export class MapPage implements OnInit {
     const map = new Map('map').setView([42.380098, -71.116629], 23); //TODO: maybe adapt zoom and starting location to trees
 
     tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
     map.invalidateSize();
 
     const customMarkerIcon = icon({
       iconUrl: '', //TODO: add tree icon and add with marker()
-      iconSize: [64, 64], 
-      popupAnchor: [0, -20]
+      iconSize: [64, 64],
+      popupAnchor: [0, -20],
     });
   }
 }
