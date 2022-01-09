15220e536a3d828adac2fb56a4a2620c7cb53452
Tobias Hölzer <tobiashoelzer@hotmail.com>
2022-01-09 17:42:32 +0100

    Create interfaces based on swagger api models

---
+++ client/src/app/interfaces/new-tree.ts
@@ -0,0 +1,9 @@
+import { CameraResultType } from '@capacitor/camera';
+import GeoInfo from './goeinfo';
+
+export default interface NewTree {
+  userName: string;
+  treeName: string;
+  geo: GeoInfo;
+  image: CameraResultType;
+}
