8b399d47a55c2681889bccaaa20db90d8db75e24
Ole Ofterdinger <ole.ofterdinger@ibm.com>
2022-01-21 17:44:46 +0100

    add body parser and increase body size limit

--- server/main.ts
+++ server/main.ts
@@ -2,11 +2,19 @@ import { Request, Response } from 'express';
 import express from 'express';
 import dotenv from 'dotenv';
 import trees from './TreeRod';
+import { TreeService } from './TreeService';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 //load environment variables
 dotenv.config();
+
+// extend maximum body limit
+app.use(express.json({ limit: '16mb' }));
+app.use(express.urlencoded({ limit: '16mb' }));
+//use body-parser middleware
+app.use(bodyParser.json());
 
 const port = process.env.PORT;
 
