7b1120f1d24926af2e7b0354376b12ad93316261
Ole Ofterdinger <ole.ofterdinger@ibm.com>
2022-01-21 12:37:42 +0100

    Implement Vote

--- server/TreeRod.ts
+++ server/TreeRod.ts
@@ -1,4 +1,9 @@
-import Express from 'express';
+import Express, { Request, Response } from 'express';
+import { query, param, validationResult } from 'express-validator';
+import { calculateNewElo } from './EloService';
+import { Tree } from './Schemas/treeSchema';
+import { WinningTree } from './interfaces/WinningTree';
+import TreeService from './TreeService';
 const router = Express.Router();
 
 router.get('/tree/:treeId', async (req: Request, res: Response) => {
@@ -11,4 +16,21 @@ router.get('/tree/:treeId', async (req: Request, res: Response) => {
   res.send(tree);
 });
 
+router.post('/vote', async (req: Request, res: Response) => {
+  query('loserId').isMongoId();
+  query('winnerId').isMongoId();
+  res.status(400).json({ errors: validationResult(req).array() });
+
+  const looserTree: Tree = await TreeService.oneTree(req.query.loserId);
+  const winnerTree: Tree = await TreeService.oneTree(req.query.winnerId);
+
+  const { newEloWinnerTree, newEloLooserTree } = calculateNewElo(
+    winnerTree.eloRating,
+    looserTree.eloRating
+  );
+  TreeService.updateScore(req.query.winnerId, newEloWinnerTree);
+  TreeService.updateScore(req.query.loserId, newEloLooserTree);
+
+  res.send(200);
+});
 export default router;
