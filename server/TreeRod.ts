1f9aae3e656307f0f34b4173ac60b67059028ec2
Ole Ofterdinger <ole.ofterdinger@ibm.com>
2022-01-21 12:13:07 +0100

    Implement get tree by id

--- server/TreeRod.ts
+++ server/TreeRod.ts
@@ -1,8 +1,14 @@
 import Express from 'express';
 const router = Express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Hello World!');
+router.get('/tree/:treeId', async (req: Request, res: Response) => {
+  param('treeId').isMongoId();
+  const tree = await TreeService.oneTree(req.params.treeId);
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  res.send(tree);
 });
 
 export default router;
