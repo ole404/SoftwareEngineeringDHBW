380cf09aa318298663598daae74752c0364303a6
Ole Ofterdinger <ole.ofterdinger@ibm.com>
2022-01-21 17:47:36 +0100

    implement random, allTrees, image and upload

--- server/TreeRod.ts
+++ server/TreeRod.ts
@@ -1,12 +1,23 @@
 import Express, { Request, Response } from 'express';
-import { query, param, validationResult } from 'express-validator';
+import { query, param, validationResult, check, body } from 'express-validator';
 import { calculateNewElo } from './EloService';
 import { Tree } from './Schemas/treeSchema';
+import { Treecognition } from './TreecognitionService';
 
 import { TreeService } from './TreeService';
 const router = Express.Router();
 
-router.get('/tree/:treeId', async (req: Request, res: Response) => {
+router.get('/', async (req: Request, res: Response) => {
+  const trees = await TreeService.getInstance().getAllTrees();
+  res.json(trees);
+});
+
+router.get('/random', async (req: Request, res: Response) => {
+  const trees = await TreeService.getInstance().getTwoRandomTrees();
+  res.send(trees);
+});
+
+router.get('/:treeId', async (req: Request, res: Response) => {
   param('treeId').isMongoId();
   const tree = await TreeService.getInstance().oneTree(req.params.treeId);
   const errors = validationResult(req);
@@ -16,9 +27,22 @@ router.get('/tree/:treeId', async (req: Request, res: Response) => {
   res.send(tree);
 });
 
-router.get('/random', async (req: Request, res: Response) => {
-  const trees = await TreeService.getInstance().getTwoRandomTrees();
-  res.send(trees);
+router.get('/image/:treeId', async (req: Request, res: Response) => {
+  param('treeId').isMongoId();
+  if (!validationResult(req).isEmpty()) {
+    return res.status(400).json('Invalid treeId');
+  }
+  const tree = (await TreeService.getInstance().oneTree(
+    req.params.treeId
+  )) as Tree;
+  res.contentType('png');
+  const image = Buffer.from(tree.image, 'base64');
+
+  res.writeHead(200, {
+    'Content-Type': 'image/png',
+    'Content-Length': image.length,
+  });
+  res.end(image);
 });
 
 router.post('/vote', async (req: Request, res: Response) => {
@@ -51,4 +75,38 @@ router.post('/vote', async (req: Request, res: Response) => {
 
   res.send(200);
 });
+
+router.post(
+  '/upload',
+  [
+    body('treeName').isString(),
+    body('userName').isString(),
+    check('image').isBase64().bail(),
+    check('image').custom(async (value) => {
+      const treeValidator = new Treecognition('gcloud.json');
+      const isTree = await treeValidator.checkForTree(value);
+      if (!isTree) {
+        throw new Error('Not a tree');
+      }
+    }),
+    check('geo.lat').isFloat(),
+    check('geo.lon').isFloat(),
+  ],
+  async (req: Request, res: Response) => {
+    const tree: Tree = req.body as Tree;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array().map((val) => ({
+          msg: val.msg,
+          location: val.location,
+          param: val.param,
+        })),
+      });
+    }
+    await TreeService.getInstance().insertTree(tree);
+    res.send(200);
+  }
+);
+
 export default router;
