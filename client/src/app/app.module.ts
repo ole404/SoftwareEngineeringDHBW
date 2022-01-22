425ae602c84dcbe04be43748e49fb89c582f11cb
Tobias Hölzer <tobiashoelzer@hotmail.com>
2022-01-22 17:46:37 +0100

    Add PWA support

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,6 +21,12 @@ import { AppRoutingModule } from './app-routing.module';
     IonicStorageModule.forRoot({
       name: '__tannder', // This is a local storage key for the entire map
       driverOrder: [Drivers.IndexedDB, Drivers.LocalStorage],
+    }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
     }),
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
